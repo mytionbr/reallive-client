2a365fdb9a9cde7d863cfca019c0faceab45b50b
matheus jesus <matheuss.jesus2017@gmail.com>
2022-04-01 17:50:45 -0300

    add signin component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthComponent } from './pages/auth/auth.component';
+import { SigninComponent } from './components/auth/signin/signin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule,
