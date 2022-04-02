71737e4b2dfffb031b30c1cd37247b59c8c4e001
matheus jesus <matheuss.jesus2017@gmail.com>
2022-04-02 03:38:21 -0300

    add handler for floating label

--- src/app/components/auth/signin/signin.component.ts
+++ src/app/components/auth/signin/signin.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-signin',
@@ -10,6 +10,23 @@ export class SigninComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  changeLabelFloat(input: HTMLInputElement){
+    const hasValue = input.value
+  
+    if(hasValue){
+      input.classList.add('active')
+    }else {
+      input.classList.remove('active')
+    }
+
+  }
+
+  onChange(event: Event){
+    const input: HTMLInputElement = event?.target as HTMLInputElement
+    console.log(input.value)
+    this.changeLabelFloat(input)
   }
 
 }
