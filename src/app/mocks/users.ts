6b9c17a4ebfa3377bf7d1fdc31be8f4caad85c87
matheus jesus <matheuss.jesus2017@gmail.com>
2022-04-25 09:35:15 -0300

    feat: add list users

--- src/app/mocks/users.ts
+++ src/app/mocks/users.ts
@@ -4,16 +4,16 @@ export const USERS: UserItem[] = [
     {
         id: '1',
         name: 'dsfsf',
-        image: '' 
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4xkkeUgUo81M4Almp1nbwm4HX858ysSa1hdGwiHYX28I5eUGI-1C61pVYZO5vFKiQBgE&usqp=CAU' 
     },
     {
         id: '1',
         name: 'dsfsf',
-        image: '' 
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4xkkeUgUo81M4Almp1nbwm4HX858ysSa1hdGwiHYX28I5eUGI-1C61pVYZO5vFKiQBgE&usqp=CAU' 
     },
     {
         id: '1',
         name: 'dsfsf',
-        image: '' 
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4xkkeUgUo81M4Almp1nbwm4HX858ysSa1hdGwiHYX28I5eUGI-1C61pVYZO5vFKiQBgE&usqp=CAU' 
     },
 ]
